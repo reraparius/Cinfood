f5e7d18116fc19c098c23fee385565af783a97bd
Annisa6780 <ilar6780@gmail.com>
2021-12-19 19:57:33 +0700

    layout menu makanan

--- Mobile App/CinFood/app/src/main/java/com/example/cinfood/API/ApiClient.java
+++ Mobile App/CinFood/app/src/main/java/com/example/cinfood/API/ApiClient.java
@@ -5,21 +5,21 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 //<<<<<<< Updated upstream
-    private static final String BASE_URL = "http://192.168.1.9/cinmobile/";
+//    private static final String BASE_URL = "http://192.168.1.9/cinmobile/";
 //=======
     //private static final String BASE_URL = "http://192.168.1.9/cinmobile/";
 //>>>>>>> Stashed changes
 
 
-    private static Retrofit retrofit;
+//    private static Retrofit retrofit;
 
-    public static Retrofit getClient() {
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-        return retrofit;
-    }
+  //  public static Retrofit getClient() {
+    //    if (retrofit == null) {
+      //      retrofit = new Retrofit.Builder()
+        //            .baseUrl(BASE_URL)
+          //          .addConverterFactory(GsonConverterFactory.create())
+            //        .build();
+        //}
+        //return retrofit;
+    //}
 }
