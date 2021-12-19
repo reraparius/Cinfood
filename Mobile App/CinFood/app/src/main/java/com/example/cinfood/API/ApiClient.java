e449c56a80c75e40289acbaab1fd1be8ad274e9a
Annisa6780 <ilar6780@gmail.com>
2021-12-19 19:18:17 +0700

    layout menu makanan

--- Mobile App/CinFood/app/src/main/java/com/example/cinfood/API/ApiClient.java
+++ Mobile App/CinFood/app/src/main/java/com/example/cinfood/API/ApiClient.java
@@ -7,7 +7,7 @@ public class ApiClient {
 //<<<<<<< Updated upstream
     private static final String BASE_URL = "http://192.168.1.9/cinmobile/";
 //=======
-    private static final String BASE_URL = "http://192.168.1.9/cinmobile/";
+    //private static final String BASE_URL = "http://192.168.1.9/cinmobile/";
 //>>>>>>> Stashed changes
 
 
