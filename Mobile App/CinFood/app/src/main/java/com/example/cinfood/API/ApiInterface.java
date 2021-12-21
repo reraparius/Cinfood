2902117fcefa0a0253a603a4c17535aee23c55df
Annisa6780 <ilar6780@gmail.com>
2021-12-21 15:31:38 +0700

    API untuk menu
    menambahkan kode file java dan api untuk menu makanan

--- Mobile App/CinFood/app/src/main/java/com/example/cinfood/API/ApiInterface.java
+++ Mobile App/CinFood/app/src/main/java/com/example/cinfood/API/ApiInterface.java
@@ -1,10 +1,14 @@
 package com.example.cinfood.API;
 
+import com.example.cinfood.Model.Menu;
 import com.example.cinfood.Model.RequestData;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiInterface {
@@ -38,5 +42,9 @@ public interface ApiInterface {
         @Field("password") String Password
 
    );
+
+    //untuk menu makanan
+    @GET("Menu_Makanan.php")
+    Call<List<Menu>> getAllMenu();
 
 }
