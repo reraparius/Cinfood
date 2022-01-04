018aac9194e5dd24365b265c61843d39b0e82d5d
Annisa6780 <ilar6780@gmail.com>
2022-01-04 11:05:27 +0700

    update Pencarian

---
+++ Mobile App/CinFood/app/src/main/java/com/example/cinfood/Main/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.cinfood.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//untuk detail menu
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
